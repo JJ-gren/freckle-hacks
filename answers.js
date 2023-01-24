85e8f56eedea31620ae03d1ffd382253ae4eda41
JJ-gren <123213839+JJ-gren@users.noreply.github.com>
2023-01-24 14:36:18 -0800

    added it
    toby sucks

---
+++ answers.js
@@ -0,0 +1,10 @@
+var id=document.getElementsByClassName("math-question__wrapper___iRtlD")[0]["dataset"]["questionId"]
+fetch("https://api.freckle.com/2/math/questions/"+id+"?lang=en", {
+  method: "GET",
+
+  headers: {
+    "Content-type": "application/json; charset=UTF-8"
+  }
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json["obfuscated-correct-answers"]));
