11e16f96c94a7e7091f3b4cd99b6f0e523b2bb6c
JJ-gren <123213839+JJ-gren@users.noreply.github.com>
2023-01-24 20:28:08 -0800

    Add files via upload

--- answers.js
+++ answers.js
@@ -1,10 +1,28 @@
-var id=document.getElementsByClassName("math-question__wrapper___iRtlD")[0]["dataset"]["questionId"]
-fetch("https://api.freckle.com/2/math/questions/"+id+"?lang=en", {
+async function yourFunction(){
+    var id=document.getElementsByClassName("math-question__wrapper___iRtlD")[0]["dataset"]["questionId"]
+var response= await fetch("https://api.freckle.com/2/math/questions/"+id+"?lang=en", {
   method: "GET",
 
   headers: {
     "Content-type": "application/json; charset=UTF-8"
   }
 })
-  .then((response) => response.json())
-  .then((json) => console.log(atob(json["obfuscated-correct-answers"])));
+json=await response.json()
+console.log(atob(json["obfuscated-correct-answers"]))
+
+var answer=atob(json["obfuscated-correct-answers"])
+var list=document.querySelectorAll('[data-mathjax="true"]')
+for (button of list) {
+    
+    if (String(button.innerText)==(answer.slice(2,answer.length-2))) {
+  button.parentElement.className="button__button___TCWNI button__secondary___WMaVW button__lg___w9Vy5 button__selectedStyle___jCcyN"
+
+  //button.parentElement.attributes[5].value="true"
+
+  //console.log(button.parentElement.attributes[5].value,button.parentElement.className)
+}
+}
+    setTimeout(yourFunction, 1000);
+}
+
+yourFunction();
