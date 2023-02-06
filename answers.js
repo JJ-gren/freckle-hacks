2b975957fb33d2c1663777f68c1494d8f21e72ea
JJ_gren <123213839+JJ-gren@users.noreply.github.com>
2023-02-06 14:42:30 -0800

    y

--- answers.js
+++ answers.js
@@ -1,36 +1,44 @@
-async function yourFunction(){
-    var id=document.getElementsByClassName("math-question__wrapper___iRtlD")[0]["dataset"]["questionId"]
-var response= await fetch("https://api.freckle.com/2/math/questions/"+id+"?lang=en", {
-  method: "GET",
-
-  headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  }
-})
-json=await response.json()
-var j=json["obfuscated-correct-answers"]
-if (typeof j =="undefined") {
-    j=json['obfuscated-fill-in-the-blanks-correct-answers']
-}
-if (typeof j =="undefined") {
-    j=json['obfuscated-graph-scatter-points-points']
-}
-
-console.log(atob(j))
-
-var answer=atob(j)
-var list=document.querySelectorAll('[data-mathjax="true"]')
-for (button of list) {
+async function getAnswer(){
+    var id = document.getElementsByClassName("math-question__wrapper___iRtlD")[0]["dataset"]["questionId"];
+    var response = await fetch("https://api.freckle.com/2/math/questions/"+id+"?lang=en", {
+      method: "GET",
     
-    if (String(button.innerText)==(answer.slice(2,answer.length-2))) {
-  button.parentElement.className="button__button___TCWNI button__secondary___WMaVW button__lg___w9Vy5 button__selectedStyle___jCcyN"
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    });
 
-  //button.parentElement.attributes[5].value="true"
+    
+    json = await response.json();
+    var answer=json["obfuscated-correct-answers"];
+    if (typeof answer == "undefined") {
+        answer=json['obfuscated-fill-in-the-blanks-correct-answers'];
+    }
+    if (typeof answer =="undefined") {
+        answer=json['obfuscated-graph-scatter-points-points']
+    }
 
-  //console.log(button.parentElement.attributes[5].value,button.parentElement.className)
+    
+    var final = JSON.parse(atob(answer));
+    console.log(final);
+    
+    var buttons = document.querySelectorAll('[data-mathjax="true"]');
+    for (var i = 0; i < buttons.length; i++){
+        for (var x = 0; x < final.length; x++){
+            if (buttons[i].innerText == final[x]){
+                buttons[i].parentElement.setAttribute("class", "button__button___TCWNI button__secondary___WMaVW button__lg___w9Vy5 button__selectedStyle___jCcyN");
+                x++;
+            }
+        }
+    }
+    
+    try {
+        var inputBar = document.getElementById("answer-input")
+        inputBar.setAttribute("placeholder", final);
+        inputBar.parentElement.setAttribute("class", "math-question-input-answer__floatingInput___Ouy10 floating-label-input__floatingLabel___cfTSY floating-label-input__floating___KA93N");
+    } catch(error) {
+        console.log();
+    }
+    getAnswer();
 }
-}
-    setTimeout(yourFunction, 1000);
-}
-
-yourFunction();
+getAnswer();
